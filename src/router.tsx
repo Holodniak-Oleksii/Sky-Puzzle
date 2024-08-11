29834420bf0e507dec277c941d2ba47e5e549b30
Oleksii Holodniak <holodniakwork@gmail.com>
2024-08-11 13:31:18 +0300

    base layout

---
+++ src/router.tsx
@@ -0,0 +1,14 @@
+import { Base } from "./components/layouts/Base";
+import { Home } from "./pages/Home";
+
+import { createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Base />,
+    children: [{ index: true, element: <Home /> }],
+  },
+]);
+
+export default router;
